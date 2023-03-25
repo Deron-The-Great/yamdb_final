b4291dd471dc569cbe6a036c4452fb6b87d85a28
Deron-The-Great <deronlgg@yandex.ru>
2023-03-25 19:27:16 +0300

    2

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -5,11 +5,9 @@ from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, permissions, status, viewsets
-from rest_framework.decorators import (action, api_view, permission_classes,
-                                       renderer_classes)
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import SAFE_METHODS
-from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
 from reviews.models import Category, Comment, Genre, Review, Title, User
